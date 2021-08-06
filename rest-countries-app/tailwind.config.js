31222c2cdd655726137e673b129d0a6b56d752b3
Mathis Tressard <mathistressard44@gmail.com>
2021-08-07 00:22:39 +0200

    add header

---
+++ rest-countries-app/tailwind.config.js
@@ -0,0 +1,33 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  mode: 'jit',
+	purge: [
+		'./src/**/*.html',
+		'./src/**/*.js'
+	],
+  darkMode: 'class',
+  theme: {
+    colors: {
+      gray: {
+        light: 'hsl(0, 0%, 100%)',
+        DEFAULT: 'hsl(0, 0%, 98%)',
+        dark: 'hsl(0, 0%, 52%)',
+      },
+      blue: {
+        light: 'hsl(209, 23%, 22%)',
+        DEFAULT: 'hsl(207, 26%, 17%)',
+        dark: 'hsl(200, 15%, 8%)',
+      },
+      current: 'currentColor',
+    },
+    fontFamily: {
+			sans: ['Nunito Sans', 'sans-serif'],
+		},
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
