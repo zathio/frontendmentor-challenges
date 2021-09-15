41799b564c78eddd6fb9ce6287f1bc0f478d4635
Mathis Tressard <mathistressard44@gmail.com>
2021-09-15 13:02:57 +0200

    style search bar

--- rest-countries-app/src/js/views/home.js
+++ rest-countries-app/src/js/views/home.js
@@ -1,19 +1,19 @@
 const html = /*html*/`
     <div class="flex items-center justify-between pb-5 md:pb-10">
-        <div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <label for="serach" class="relative w-full max-w-[29rem]">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 select-none pointer-events-none absolute left-8 top-1/2 -translate-y-1/2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
             </svg>
-            <input type="search" id="search" placeholder="Search for a country..." class="bg-blue-light placeholder-current">
-        </div>
+            <input type="search" id="search" placeholder="Search for a country..." class="rounded-md shadow-md bg-blue-light py-4 px-5 pl-20 w-full placeholder-current outline-none ring-gray-dark/40 dark:ring-gray-light/40 lg:hover:bg-gray-dark/10 lg:dark:hover:bg-gray-light/10 active:bg-gray-dark/10 dark:active:bg-gray-light/10 active:ring-2 focus:ring-2 duration-100">
+        </label>
         <div id="filter" class="relative">
-            <button class="flex items-center justify-between select-none rounded-md bg-blue-light py-4 px-5 w-52">
+            <button class="flex items-center justify-between select-none rounded-md shadow-md bg-blue-light py-4 px-5 w-52 ring-gray-dark/40 dark:ring-gray-light/40 lg:hover:bg-gray-dark/10 lg:dark:hover:bg-gray-light/10 active:bg-gray-dark/10 dark:active:bg-gray-light/10 active:ring-2 focus:ring-2 duration-100">
                 <span id="name">Filter by Region</span>
                 <svg id="icon" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 duration-100" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                 </svg>
             </button>
-            <ul class="invisible opacity-0 duration-100 absolute top-full left-0 mt-1 w-full rounded-md bg-blue-light py-3">
+            <ul class="invisible opacity-0 duration-100 absolute top-full left-0 mt-1 w-full rounded-md shadow-md bg-blue-light py-3">
                 <li class="select-none py-1 px-5 cursor-pointer hover:bg-gray-dark">All</li>
                 <li class="select-none py-1 px-5 cursor-pointer hover:bg-gray-dark">Africa</li>
                 <li class="select-none py-1 px-5 cursor-pointer hover:bg-gray-dark">Americas</li>
