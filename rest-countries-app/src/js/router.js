6ada3ecdd9fcf80db8ac4869cf3c4177b1c627e9
Mathis Tressard <mathistressard44@gmail.com>
2021-09-23 14:17:28 +0200

    fix routing

--- rest-countries-app/src/js/router.js
+++ rest-countries-app/src/js/router.js
@@ -1,18 +1,21 @@
 import home from "./views/home.js";
 import detail from "./views/detail.js";
 
-const app = document.querySelector("#app");
+const app = document.querySelector("#app"),
+      formatTitle = str => decodeURI(str.charAt(0).toUpperCase() + str.slice(1));
 
 function router() {
-    document.title = location.pathname.split("/")[1] || "Countries App";
     location.pathname === "/" ? home(app) : detail(app);
+    document.title = formatTitle(location.pathname.split("/")[1]) || "Countries App";
+    history.replaceState("", "", document.title);
 };
 
 // Handle navigation
 window.addEventListener("click", e => {
-    if (e.target.matches("a[data-country]")) {
+    if (e.target.closest("a[data-link]")) {
         e.preventDefault();
-        history.pushState("", "", e.target.href);
+        history.replaceState("", "", e.target.closest("a[data-link]").href);
+        router();
     }
 });
 
--- rest-countries-app/src/js/views/detail.js
+++ rest-countries-app/src/js/views/detail.js
@@ -1,15 +1,23 @@
 const html = {
+    skeleton: () => /*html*/`
+
+    `,
     base: country => /*html*/`
+        <a href="/" data-link>Back</a>
         <h1>${country.name}</h1>
     `
 };
 
 export default app => {
+    app.innerHTML = html.skeleton();
+
     // Get datas
     fetch("https://restcountries.com/v2/name" + location.pathname)
         .then(res => res.json())
         .then(data => {
             app.innerHTML = html.base(data[0]);
         })
-    .catch(console.error);
+    .catch(() => {
+        document.querySelector("[href='/']").click();
+    });
 }
