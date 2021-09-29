d629a31e2a89d3ae5acc7a1a840abacd8e001db1
Mathis Tressard <mathistressard44@gmail.com>
2021-09-29 19:56:09 +0200

    start responsive

--- rest-countries-app/src/js/views/detail.js
+++ rest-countries-app/src/js/views/detail.js
@@ -1,6 +1,6 @@
 const html = {
     skeleton: () => /*html*/`
-        <a href="/" data-link class="inline-flex items-center select-none rounded-md shadow-md bg-gray-light dark:bg-blue-light my-6 py-2 px-10 ring-gray-dark/40 dark:ring-gray-light/40 hover:bg-gray-dark/10 dark:hover:bg-gray-light/10 active:bg-gray-dark/10 dark:active:bg-gray-light/10 active:ring-2 focus:ring-2 outline-none duration-100 text-lg">
+        <a href="/" data-link class="inline-flex items-center select-none rounded-md shadow-md bg-gray-light dark:bg-blue-light my-6 py-1.5 lg:py-2 px-8 lg:px-10 ring-gray-dark/40 dark:ring-gray-light/40 hover:bg-gray-dark/10 dark:hover:bg-gray-light/10 active:bg-gray-dark/10 dark:active:bg-gray-light/10 active:ring-2 focus:ring-2 outline-none duration-100 text-lg">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
             </svg>
@@ -8,12 +8,12 @@ const html = {
         </a>
     `,
     base: country => /*html*/`
-        <div class="flex items-center mt-10">
-            <img src="${country.flags[0]}" alt="Flag" class="w-1/2 shadow-lg">
-            <div class="w-1/2">
-                <h1>${country.name}</h1>
-                <div class="grid grid-cols-2 mt-6 mb-16">
-                    <ul>
+        <div class="px-3 sm:px-0 lg:flex lg:justify-between lg:items-center lg:gap-x-20 xl:gap-x-32 mt-10">
+            <img src="${country.flag}" alt="Flag" class="object-cover lg:w-1/2 shadow-lg mx-auto mb-14 lg:mb-0" style="aspect-ratio: 4/3">
+            <div class="lg:w-1/2">
+                <h1 class="font-bold text-[2rem]">${country.name}</h1>
+                <div class="text-lg lg:text-base grid sm:grid-cols-2 gap-x-6 justify-between mt-6 mb-16">
+                    <ul class="space-y-2">
                         <li>
                             <span class="font-semibold">Native Name:</span>
                             ${get(country.nativeName)}
@@ -24,18 +24,18 @@ const html = {
                         </li>
                         <li>
                             <span class="font-semibold">Region:</span>
-                            ${get(country.continent)}
+                            ${get(country.region)}
                         </li>
                         <li>
                             <span class="font-semibold">Sub Regions:</span>
-                            ${get(country.region)}
+                            ${get(country.subregion)}
                         </li>
                         <li>
                             <span class="font-semibold">Capital:</span>
                             ${get(country.capital)}
                         </li>
                     </ul>
-                    <ul>
+                    <ul class="mt-10 lg:mt-0 space-y-2">
                         <li>
                             <span class="font-semibold">Top Level Domain:</span>
                             ${get(country.topLevelDomain)}
@@ -50,32 +50,55 @@ const html = {
                         </li>
                     </ul>
                 </div>
-                <span class="font-semibold">Border Countries:</span>
-                ${get(country.borders)}
+                <div>
+                    <span class="font-semibold mr-4 sm:float-left">Border Countries:</span>
+                    <div id="border-box" class="mt-6 sm:-mt-1 ">
+                        <div class="inline-block mb-2 w-[6.6rem] h-7 bg-gray-dark/60 rounded-md mr-1.5"></div>
+                        <div class="inline-block mb-2 w-[6.6rem] h-7 bg-gray-dark/60 rounded-md mr-1.5"></div>
+                        <div class="inline-block mb-2 w-[6.6rem] h-7 bg-gray-dark/60 rounded-md mr-1.5"></div>
+                    </div>
+                </div>
             </div>
         </div>
+    `,
+    border: name => /*html*/`
+        <a href="/${name}" class="inline-block -mt-1 mb-2 text-center rounded-md shadow-md bg-gray-light dark:bg-blue-light whitespace-nowrap py-[.3rem] px-2 w-[6.6rem] overflow-hidden overflow-ellipsis ring-gray-dark/40 dark:ring-gray-light/40 hover:bg-gray-dark/10 dark:hover:bg-gray-light/10 active:bg-gray-dark/10 dark:active:bg-gray-light/10 active:ring-2 focus:ring-2 outline-none duration-100 mr-1.5">${name}</a>
     `
 };
 
+// Treat each datas types
 function get(data) {
     if (!data) return "Unknown";
-    if (Array.isArray(data)) return data.map(d => `<a href="/${d}">${d}</a>`).join("");
     if (typeof data[0] === "object") return data.map(d => d.name).join(", ");
+    if (Array.isArray(data)) return data.join(", ");
     return data;
+};
+
+function test(codes) {
+    let box = app.querySelector("#border-box"), first = true;
+
+    codes.forEach(c => {
+        fetch("https://restcountries.com/v2/alpha/" + c)
+        .then(res => res.json())
+        .then(data => {
+            if (first) box.innerHTML = "", first = false;
+            box.insertAdjacentHTML("beforeend", html.border(data.name));
+        });
+    });
 };
 
 export default app => {
     app.innerHTML = html.skeleton();
-
+    
     // Get datas
     fetch("https://restcountries.com/v2/name" + location.pathname)
         .then(res => res.json())
         .then(data => {
-            console.log(data[0]);
             app.insertAdjacentHTML("beforeend", html.base(data[0]));
+            test(data[0].borders);
         })
     .catch(() => {
         // Go to home page if error in the request
-        document.querySelector("[href='/']").click();
+        // document.querySelector("[href='/']").click();
     });
 }
--- rest-countries-app/src/js/views/home.js
+++ rest-countries-app/src/js/views/home.js
@@ -1,14 +1,14 @@
 const html = {
     base: () => /*html*/`
-        <div class="flex items-center justify-between pb-5 md:pb-10 text-sm">
+        <div class="flex flex-col sm:flex-row sm:items-center gap-4 justify-between pb-5 md:pb-10 text-sm">
             <label for="search" class="relative w-full max-w-[29rem] text-gray-dark dark:text-gray-light">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 select-none pointer-events-none absolute left-7 top-1/2 -translate-y-1/2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
                 <input type="search" id="search" placeholder="Search for a country..." class="rounded-md shadow-md bg-gray-light dark:bg-blue-light py-4 px-5 pl-[4.5rem] w-full placeholder-current outline-none ring-gray-dark/40 dark:ring-gray-light/40 hover:bg-gray-dark/10 dark:hover:bg-gray-light/10 active:bg-gray-dark/10 dark:active:bg-gray-light/10 active:ring-2 focus:ring-2 duration-100">
             </label>
-            <div id="filter" class="relative">
-                <button class="flex items-center justify-between select-none rounded-md shadow-md bg-gray-light dark:bg-blue-light py-4 px-5 w-48 ring-gray-dark/40 dark:ring-gray-light/40 hover:bg-gray-dark/10 dark:hover:bg-gray-light/10 active:bg-gray-dark/10 dark:active:bg-gray-light/10 active:ring-2 focus:ring-2 outline-none duration-100">
+            <div id="filter" class="relative w-48 flex-shrink-0">
+                <button class="flex items-center justify-between select-none rounded-md shadow-md bg-gray-light dark:bg-blue-light py-4 px-5 w-full ring-gray-dark/40 dark:ring-gray-light/40 hover:bg-gray-dark/10 dark:hover:bg-gray-light/10 active:bg-gray-dark/10 dark:active:bg-gray-light/10 active:ring-2 focus:ring-2 outline-none duration-100">
                     <span id="name" data-region="">Filter by Region</span>
                     <svg id="icon" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 duration-100 -mr-px" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -19,7 +19,7 @@ const html = {
                 </ul>
             </div>
         </div>
-        <ul id="countries-list" class="list-none grid grid-cols-4 gap-16">
+        <ul id="countries-list" class="list-none px-3 sm:px-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 xl:gap-16">
             ${ /*html*/`
                 <li class="block rounded-md overflow-hidden bg-gray-light dark:bg-blue-light shadow-md h-[22rem] animate-pulse">
                     <div class="bg-gray-dark/60 w-full h-[47%]"></div>
@@ -37,8 +37,8 @@ const html = {
     `,
     card: country => /*html*/`
         <li>
-            <a href="/${country.name}" data-link data-country="${country.name}" data-region="${country.continent}" class="block h-[22rem] rounded-md overflow-hidden outline-none bg-gray-light dark:bg-blue-light shadow-md hover:bg-gray-dark/10 hover:-translate-y-1 will-change dark:hover:bg-gray-light/10 active:bg-gray-dark/20 dark:active:bg-gray-light/20 active:translate-y-0 ring-gray-dark/40 dark:ring-gray-light/40 focus-visible:ring duration-100">
-                <img src="${country.flags[0]}" alt="Flag" loading="lazy" class="w-full h-[47%] object-cover bg-gray-dark/60">
+            <a href="/${country.name}" data-link data-country="${country.name}" data-region="${country.region}" class="block h-[22rem] rounded-md overflow-hidden outline-none bg-gray-light dark:bg-blue-light shadow-md hover:bg-gray-dark/10 hover:-translate-y-1 will-change dark:hover:bg-gray-light/10 active:bg-gray-dark/20 dark:active:bg-gray-light/20 active:translate-y-0 ring-gray-dark/40 dark:ring-gray-light/40 focus-visible:ring duration-100">
+                <img src="${country.flag}" alt="Flag" loading="lazy" class="w-full h-1/2 lg:h-[47%] object-cover bg-gray-dark/60">
                 <div class="p-6">
                     <p class="font-bold text-lg whitespace-nowrap overflow-hidden overflow-ellipsis">${country.name}</p>
                     <ul class="text-sm space-y-1.5 mt-4">
@@ -48,7 +48,7 @@ const html = {
                         </li>
                         <li>
                             <span class="font-semibold">Region:</span>
-                            ${country.continent || "Unknown"}
+                            ${country.region || "Unknown"}
                         </li>
                         <li>
                             <span class="font-semibold">Capital:</span>
@@ -112,7 +112,7 @@ export default app => {
             
             data.forEach(d => {
                 countriesList.insertAdjacentHTML("beforeend", html.card(d));
-                !optionsList.includes(d.continent) && optionsList.push(d.continent);
+                !optionsList.includes(d.region) && optionsList.push(d.region);
             });
 
             optionsList.sort() && optionsList.forEach(o => o && options.insertAdjacentHTML("beforeend", html.option(o)));
